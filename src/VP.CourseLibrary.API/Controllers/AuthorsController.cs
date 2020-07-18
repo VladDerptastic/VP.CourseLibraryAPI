3cbfe6fc3f2e308b4a4c17fcd2d6ffd906e548c3
Derptastic <sheyki@abv.bg>
2020-07-18 18:44:25 +0200

    Added - CoursesController with implementation for all courses and for a specific course - using the Dto objects. Refactored - AuthorsController to use Dto objects over direct entities.

--- src/VP.CourseLibrary.API/Controllers/AuthorsController.cs
+++ src/VP.CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,6 +1,9 @@
-﻿using CourseLibrary.API.Services;
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using VP.CourseLibrary.API.DtoModels;
 
 namespace VP.CourseLibrary.API.Controllers
 {
@@ -9,31 +12,35 @@ namespace VP.CourseLibrary.API.Controllers
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
 
-        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
         {
             _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
         [HttpGet()]
-        public IActionResult GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
-            var authors = _courseLibraryRepository.GetAuthors();
+            var efAuthors = _courseLibraryRepository.GetAuthors();
+
             //return JsonResult(authors);
-            //No NotFound codes => if we connect to this method, we're getting an existing resource on the DB even if its empty table
-            return Ok(authors);
+            //Don't use NotFound codes => if we connect to this method, we're getting an existing resource on the DB even if its empty table
+            //NotFound would be confusing
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(efAuthors));
         }
 
         [HttpGet("{authorId:guid}")] 
         //[HttpGet("{authorId:int}")] => if we had an int ID as well as a guid, we could use this on another method => Route Constraints
-        public IActionResult GetAuthor(Guid authorId)
+        public ActionResult<AuthorDto> GetAuthor(Guid authorId)
         {
             var author = _courseLibraryRepository.GetAuthor(authorId);
 
             if (author == null)
                 return NotFound();
 
-            return Ok(author);
+            return Ok(_mapper.Map<AuthorDto>(author));
         }
     }
 }
---
+++ src/VP.CourseLibrary.API/Controllers/CoursesController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VP.CourseLibrary.API.DtoModels;
+
+namespace VP.CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors/{authorId}/courses")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
+        {
+            if (_courseLibraryRepository.AuthorExists(authorId) == false)
+                return NotFound();
+            
+            var coursesForAuthor = _courseLibraryRepository.GetCourses(authorId);
+            return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthor));
+        }
+
+        [HttpGet("{courseId}")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (_courseLibraryRepository.AuthorExists(authorId) == false)
+                return NotFound();
+
+            var course = _courseLibraryRepository.GetCourse(authorId, courseId);
+
+            if (course == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<CourseDto>(course));
+        }
+    }
+}
